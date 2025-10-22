ad1a7e140ebe7f9699bc9af8de4732619be0947b
Javier <javninpen@alu.edu.gva.es>
2025-10-22 19:54:55 +0200

    Generacion Estructura Proyecto TDD

---
+++ Actividad1TDD/TestActividad1TDD/FuncionesTest.cs
@@ -0,0 +1,11 @@
+﻿namespace TestActividad1TDD
+{
+    [TestClass]
+    public sealed class FuncionesTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
