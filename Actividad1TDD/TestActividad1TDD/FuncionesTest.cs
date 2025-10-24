10638bd7677885ab7f12f4a14f45231c8edb083f
Javier <javninpen@alu.edu.gva.es>
2025-10-24 10:12:51 +0200

    Se añaden dos nuevos métodos de prueba para verificar el comportamiento de la función `CalcularFactorial` con entradas de 0 y 5, mejorando así la cobertura de pruebas.

--- Actividad1TDD/TestActividad1TDD/FuncionesTest.cs
+++ Actividad1TDD/TestActividad1TDD/FuncionesTest.cs
@@ -1,11 +1,29 @@
-﻿namespace TestActividad1TDD
+﻿using Actividad1TDD;
+
+namespace TestActividad1TDD
 {
     [TestClass]
     public sealed class FuncionesTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CalcularFactorial_DeberiaDevolverMenosUno_SiNumeroEsNegativo()
         {
+            long resultado = Funciones.CalcularFactorial(-5);
+            Assert.AreEqual(-1, resultado, "El factorial de un numero negativo deberia devolver -1");
+        }
+
+        [TestMethod]
+        public void CalcularFactorial_DeberiaDevolverUno_SiNumeroEsCero()
+        {
+            long resultado = Funciones.CalcularFactorial(0);
+            Assert.AreEqual(1, resultado, "El factorial de 0 deberia devolver 1");
+        }
+
+        [TestMethod]
+        public void CalcularFactorial_Correcto()
+        {
+            long resultado = Funciones.CalcularFactorial(5);
+            Assert.AreEqual(5, resultado, "El factorial de 5 deberia devolver 120");
         }
     }
 }
